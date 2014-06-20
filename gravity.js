320a3477766222296d0d3096a1c6203d65b27b37
lexjacobs <lex.jacobs@gmail.com>
2014-06-20 01:04:52 -0700

    Adds 2nd element

--- gravity.js
+++ gravity.js
@@ -2,52 +2,20 @@
 
 var FallingObject = function(height){
 
-  var height = height;
+  this.height = height;
 
-  var startTime = new Date();
+  this.startTime = new Date();
 
   var g = 9.80665;
 
   this.currentHeight = function(){
 
     // in seconds
-    var timeElapsed = (new Date() - startTime) / 1000;
+    this.timeElapsed = (new Date() - startTime) / 1000;
 
-    var fallen = g * Math.pow(timeElapsed, 2) / 2
+    this.fallen = g * Math.pow(this.timeElapsed, 2) / 2
 
-    return height - fallen;
-  }
-
-  this.stats = function(){
-    console.log('currentHeight: ', this.currentHeight().toFixed(2));
+    return this.height - this.fallen;
   }
 
 };
-
-// var orange = new FallingObject(316);
-
-// console.log(orange.currentHeight());
-// setTimeout(function(){
-//   console.log(orange.stats());
-// }, 1000)
-// setTimeout(function(){
-//   console.log(orange.stats());
-// }, 2000)
-// setTimeout(function(){
-//   console.log(orange.stats());
-// }, 3000)
-// setTimeout(function(){
-//   console.log(orange.stats());
-// }, 4000)
-// setTimeout(function(){
-//   console.log(orange.stats());
-// }, 5000)
-// setTimeout(function(){
-//   console.log(orange.stats());
-// }, 6000)
-// setTimeout(function(){
-//   console.log(orange.stats());
-// }, 7000)
-// setTimeout(function(){
-//   console.log(orange.stats());
-// }, 8000)
