1eea0ec8568a357e3e58287d68e356da02cc55f5
lexjacobs <lex.jacobs@gmail.com>
2014-06-25 00:01:59 -0700

    Refactors to pass board into helper functions

--- app/script/obstacleGenerator.js
+++ app/script/obstacleGenerator.js
@@ -1,24 +1,26 @@
 // generates bricks
-$obstacleGen = function(){
+$obstacleGen = function(board){
 
   var randomNumber = Math.random();
 
   // generates obstacles in the first row
   if(randomNumber < 0.5){
-    var rows = $board.board.length;
-    var cols = $board.board[0].length;
+    var rows = board.length;
+    var cols = board[0].length;
     var positionOfObstacle = Math.floor(Math.random()*cols);
-    $board.board[rows-1][positionOfObstacle] = 'b';
+    board[rows-1][positionOfObstacle] = 'b';
   }
 };
 
 // advances obstacles
-$obstacleAdvance = function(){
-  $collsionDetect();
-  for (var i = 1; i < $board.board.length-1; i++) {
-    for (var j = 0; j < $board.board[i].length; j++) {
-      $board.board[i][j] = $board.board[i+1][j];
-      $board.board[i+1][j] = 'x';
+$obstacleAdvance = function(board){
+  if($collisionDetect(board)){
+    console.log('bonk!');
+  };
+  for (var i = 1; i < board.length-1; i++) {
+    for (var j = 0; j < board[i].length; j++) {
+      board[i][j] = board[i+1][j];
+      board[i+1][j] = 'x';
     }
   }
   $render();
@@ -26,17 +28,19 @@ $obstacleAdvance = function(){
 
 // set interval of obstacle generation once per second
 $interval(function(){
-  $obstacleGen();
+  $obstacleGen($board.board);
   $render();
 }, 1000);
 
 // detects non-empty squares below apple before advancing row
-$collsionDetect = function() {
-  for(var i = 0; i < $board.board[0].length; i++ ){
-    if($board.board[0][i] === 'o'){
-      if($board.board[1][i] !== 'x'){
-        console.log('collision detected!');
+$collisionDetect = function(board) {
+  for(var i = 0; i < board[0].length; i++ ){
+    if(board[0][i] === 'o'){
+      if(board[1][i] !== 'x'){
+        // console.log('collision detected!');
+        return true;
       }
     }
   }
+  return false;
 }
