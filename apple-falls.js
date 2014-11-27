be59fb39eebb8c398f54f0be349131f9e8f7ed73
lexjacobs <lex.jacobs@gmail.com>
2014-11-26 22:26:22 -0800

    Refactors logic around parachute deployment.

--- apple-falls.js
+++ apple-falls.js
@@ -1,82 +1,84 @@
-var BoardMaker = function(nX, nY){
+var BoardMaker = function(nX, nY) {
 
-  this.board = [];
-  
-  for(var i = 0; i < nY; i++){
-    this.board.push([]);
-  }
+    this.board = [];
 
-  for(var j = 0; j < this.board.length; j++){
-    for(var k = 0; k < nX; k++){
-      this.board[j].push('x');
+    for (var i = 0; i < nY; i++) {
+        this.board.push([]);
     }
-  }
+
+    for (var j = 0; j < this.board.length; j++) {
+        for (var k = 0; k < nX; k++) {
+            this.board[j].push('x');
+        }
+    }
 };
 
-BoardMaker.prototype.moveRight = function(){
-  for(var i = 0; i < this.board[0].length; i++){
+BoardMaker.prototype.moveRight = function() {
+    for (var i = 0; i < this.board[0].length; i++) {
 
-    if(this.board[0][i] === 'o' && i < this.board[0].length-1){
-      this.board[0][i] = 'x';
-      this.board[0][i+1] = 'o';
-      break;
+        if (this.board[0][i] === 'o' && i < this.board[0].length - 1) {
+            this.board[0][i] = 'x';
+            this.board[0][i + 1] = 'o';
+            break;
+        }
+        if (this.board[0][i] === 'p' && i < this.board[0].length - 1) {
+            this.board[0][i] = 'x';
+            this.board[0][i + 1] = 'p';
+            break;
+        }
     }
-    if(this.board[0][i] === 'p' && i < this.board[0].length-1){
-      this.board[0][i] = 'x';
-      this.board[0][i+1] = 'p';
-      break;
-    }
-  }
-  return;
+    return;
 };
 
-BoardMaker.prototype.moveLeft = function(){
-  for(var i = 0; i < this.board[0].length; i++){
-    if(this.board[0][i] === 'o' && i !== 0){
-      this.board[0][i] = 'x';
-      this.board[0][i-1] = 'o';
-      break;
+BoardMaker.prototype.moveLeft = function() {
+    for (var i = 0; i < this.board[0].length; i++) {
+        if (this.board[0][i] === 'o' && i !== 0) {
+            this.board[0][i] = 'x';
+            this.board[0][i - 1] = 'o';
+            break;
+        }
+        if (this.board[0][i] === 'p' && i !== 0) {
+            this.board[0][i] = 'x';
+            this.board[0][i - 1] = 'p';
+            break;
+        }
     }
-    if(this.board[0][i] === 'p' && i !== 0){
-      this.board[0][i] = 'x';
-      this.board[0][i-1] = 'p';
-      break;
-    }
-  }
-  return;
+    return;
 };
 
-BoardMaker.prototype.deploy = function(){
-  if($parachuteCount === 0){
+BoardMaker.prototype.deploy = function() {
+    var timeoutFlag = false;
+    if ($parachuteCount === 0) {
+        return false;
+    }
+    for (var i = 0; i < this.board[0].length; i++) {
+        if (this.board[0][i] === 'o') {
+            this.board[0][i] = 'p';
+            $parachuteCount--;
+            $timeDecreaser.current = 1;
+            timeoutFlag = true;
+            break;
+        }
+    }
+    if (timeoutFlag) {
+        var self = this;
+        setTimeout(function() {
+            self.undeploy();
+        }, 3000);
+    }
     return false;
-  }
- 
-  for(var i = 0; i < this.board[0].length; i++){
-    if(this.board[0][i] === 'o'){
-      this.board[0][i] = 'p';
-
-      var that = this;
-      setTimeout(function(){
-        that.undeploy();
-      },3000);
-
-      $parachuteCount--;
-      $timeDecreaser.current = 1;
-      break;
-    }
-  }
-  return false; 
 };
 
-BoardMaker.prototype.undeploy = function(){
- for(var i = 0; i < this.board[0].length; i++){
-    if(this.board[0][i] === 'p'){
-      this.board[0][i] = 'o';
-      break;
+BoardMaker.prototype.undeploy = function() {
+    for (var i = 0; i < this.board[0].length; i++) {
+        if (this.board[0][i] === 'p') {
+            this.board[0][i] = 'o';
+            break;
+        }
     }
-  }
-  return true; 
+    return true;
 };
+
 $firstRun = true;
 
 ($init = function(){
@@ -223,28 +225,6 @@ $firstRun = true;
 
   $('.status').text('');
 })();
-// http://www.gravitycalc.com/
-
-var FallingObject = function(height, g){
-
-  var height = height;
-
-  var startTime = new Date();
-
-  g = (g * 10);
-
-  this.currentHeight = function(){
-
-    // in seconds
-    var timeElapsed = (new Date() - startTime) / 1000;
-
-    var fallen = g * Math.pow(timeElapsed, 2) / 2;
-
-    return height - fallen;
-  };
-
-};
-
 var keyRight = function(){
   $sideCollisionDetectRight($board.board);
   $board.moveRight();
